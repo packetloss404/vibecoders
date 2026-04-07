70b4ab4b6cf777da21ac7d065e85cc9f54abe47c
packetloss404 <packetloss404@users.noreply.github.com>
2026-04-07 10:37:47 -0500

    Initial vibe coders site

---
+++ app/data/streamers.ts
@@ -0,0 +1,33 @@
+export interface Streamer {
+  name: string;
+  channelUrl: string;
+  schedule?: string;
+  bio?: string;
+}
+
+export const streamers: Streamer[] = [
+  {
+    name: "dyoburon",
+    channelUrl: "https://youtube.com/@dyoburon",
+  },
+  {
+    name: "HardHeadHackerHead",
+    channelUrl: "https://youtube.com/@HardHeadHackerHead",
+  },
+  {
+    name: "BilbroSwagginzCoding",
+    channelUrl: "https://youtube.com/@BilbroSwagginzCoding",
+  },
+  {
+    name: "Clearmud",
+    channelUrl: "https://youtube.com/@Clearmud",
+  },
+  {
+    name: "khuurdotdev",
+    channelUrl: "https://youtube.com/@khuurdotdev",
+  },
+  {
+    name: "bridgemindai",
+    channelUrl: "https://youtube.com/@bridgemindai",
+  },
+];
